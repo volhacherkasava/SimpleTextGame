c1fa19b6d6211feb82d0accfbbe6ef02ea544452
Siarhei Frunchak <Siarhei_frunchak@epam.com>
2018-06-15 17:58:16 +0300

    #First build
    IoManager with simple Write and Read methods are implemented

---
+++ ConsoleApp1/ConsoleApp1/IoManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTextGame
+{
+    //it's class implemented for work with console text outputs and inputs
+    // Created by Siarhei Frunchak
+    public class IoManager
+    {
+        //Read line method
+        public void Read()
+        {
+            Console.ReadLine();
+        }
+        //Write method
+        public void Write(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
