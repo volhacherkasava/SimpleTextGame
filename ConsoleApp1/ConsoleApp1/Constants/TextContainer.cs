b25f80b612ecdd7ae75306af42783210534ec8c7
volhacherkasava <40197660+volhacherkasava@users.noreply.github.com>
2018-06-18 13:01:00 +0300

    Update TextContainer.cs

--- ConsoleApp1/ConsoleApp1/Constants/TextContainer.cs
+++ ConsoleApp1/ConsoleApp1/Constants/TextContainer.cs
@@ -4,5 +4,6 @@
     public class TextContainer
     {
         public const string earthText = "Our planet has existed for 4.5 billion years, and it has been a\n busy few eons.Here are the 25 biggest milestones in Earth's\n history. From leaps forward in evolution to devastating asteroid\n impacts, these were the turning points that shaped our world";
+         public const string gravityText = "We, humans, live on the Earth. And our planet Earth is surrounded\n by space. The sky is what we see of space. There are billions of stars\n and probably billions of other planets moving through space.\n Our planet is moving through space too. We live on the surface \n of our planet Earth. Gravity keeps us here on the Earth.\n Gravity is an invisible force that pulls things and people toward the \n center of the Earth. All the other planets, stars and moons have \n gravity too. Without gravity we would float away into space.";
     }
 }
